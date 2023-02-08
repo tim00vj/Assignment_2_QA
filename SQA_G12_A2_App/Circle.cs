732a92e4928d74ee1512f652bb0e3186e22978be
Anusuya Rajamani <Arajamani8366@conestogac.on.ca>
2023-02-08 15:08:38 -0500

    SQA_G12_A2_APP

---
+++ SQA_G12_A2_App/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQA_G12_A2_App
+{
+    public class Circle
+    {
+        public int radius;
+        public Circle()
+        {
+            radius = 1;
+        }
+
+        public Circle(int user_radius)
+        {
+            radius = user_radius;
+        }
+
+        public int GetCircleRadius()
+        {
+
+            return this.radius;
+        }
+        public void ChangeCircleRadius(int user_radius)
+        {
+
+            this.radius = user_radius;
+        }
+
+        public double GetCircleCircumference()
+        {
+            return 2 * 3.14 * radius;
+        }
+        public double GetCircleArea()
+        {
+            return 3.14 * radius * radius;
+        }
+    }
+}
---
+++ SQA_G12_A2_App/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQA_G12_A2_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Console.WriteLine("Please enter the radius of the circle");
+            //int ru = Convert.ToInt32(Console.ReadLine());
+            int ru = 0;
+            Circle c = new Circle();
+            // Console.WriteLine("this is the radius you entered "+c.GetRadius());
+            int userinput;
+            do
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine("1.Set Radius");
+                Console.WriteLine("2.Update Radius");
+
+
+                Console.WriteLine("3.Get Area");
+                Console.WriteLine("4.Get Circumference");
+                Console.WriteLine("5.Exit");
+                Console.WriteLine("Please select a menu");
+                userinput = Convert.ToInt32(Console.ReadLine());
+                if (userinput == 1)
+                {
+                    Console.WriteLine("Please enter the radius of the circle\n");
+                    ru = Convert.ToInt32(Console.ReadLine());
+                    c.ChangeCircleRadius(ru);
+                }
+                else if (userinput == 2)
+                {
+                    // Console.WriteLine("The radius value is" + c.GetRadius());
+                    Console.WriteLine("Please enter the radius of the circle\n");
+                    ru = Convert.ToInt32(Console.ReadLine());
+                    c.ChangeCircleRadius(ru);
+                }
+                else if (userinput == 3)
+                {
+                    Console.WriteLine("The radius value is " + c.GetCircleArea());
+                }
+
+                else if (userinput == 4)
+                {
+                    Console.WriteLine("The radius value is " + c.GetCircleCircumference());
+                }
+
+                else if (userinput == 5)
+                {
+                    break;
+                }
+
+
+            } while (userinput != 5);
+        }
+
+    }
+}
